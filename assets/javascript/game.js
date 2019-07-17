5b6f844c134d1679802d47c835ccdc7e4129e513
Cesar Silva <silvacde@gmail.com>
2019-07-16 22:37:06 -0700

    function to generate random number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,3 +23,19 @@ var crystal = {
     }
   };
   
+  // Scores (Current and Target)
+  var currentScore = 0;
+  var targetScore = 0;
+  
+  // Wins and Losses
+  var winCount = 0;
+  var lossCount = 0;
+
+  // FUNCTIONS
+  // =================================================================
+  
+  // Helper Function for getting random numbers
+  var getRandom = function(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+  
