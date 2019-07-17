82458139eb6295c563d0718be2d68728658264c4
Cesar Silva <silvacde@gmail.com>
2019-07-16 20:22:16 -0700

    added variable to change the current number

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,12 @@
 // Execute this code when the DOM has fully loaded.
 
 $(document).ready(function() {
+    
+    // The number we will change repeatedly by clicking crystals. This will serve as "current guess" number.
+    var yourMatchingNumber = 0;
 
-})
+  
+
+}
+
+);
