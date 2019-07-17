a5dd36bee58dbb96be645a99ce293c36c5de50f9
Cesar Silva <silvacde@gmail.com>
2019-07-16 20:25:49 -0700

    added pseudo code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,12 @@ $(document).ready(function() {
     
     // The number we will change repeatedly by clicking crystals. This will serve as "current guess" number.
     var yourMatchingNumber = 0;
+// Create a variable that will generate a random "target number".
+ 
+  // Variables needed to start the game.
 
+
+// Create a function that will generate a random value for the crystals 
   
 
 }
