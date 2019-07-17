fe3ddfbb20a5a33f78d6a565e62fef7cf01ceb61
Cesar Silva <silvacde@gmail.com>
2019-07-16 22:23:55 -0700

    variables for crystals

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,56 +1,25 @@
-// Execute this code when the DOM has fully loaded.
 
-$(document).ready(function() {
-    
-    // The number we will change repeatedly by clicking crystals. This will serve as "current guess" number.
-    var yourMatchingNumber = 0;
-// Create a variable that will generate a random "target number".
- 
-var randomNum = randomNumGen();
-
-  // Variables needed to start the game.
-
-  var wins = 0;
-  var losses = 0;
-  var crystals;
-  
-   
-// Create a function that will generate a random value for the crystals and returns our crystals object.
-
-function randomNumCrystals() {
-
-
-// Create the crystals object.
-
-return {
-    red: {
-            
-    points: Math.floor(Math.random() * 12) + 1,
-            
-    imageUrl: "../../images/RedCrystal.jpeg"
-          
+// Variables for Crystals
+var crystal = {
+    blue:
+    {
+      name: "Blue",
+      value: 0
     },
-    blue: {
-        
-        points: Math.floor(Math.random() * 12) + 1,
-                
-        imageUrl: "assets/images/blue.png"
-              
-        },
-              
-        yellow: {
-                
-        points: Math.floor(Math.random() * 12) + 1,
-                
-        imageUrl: "assets/images/yellow.png"
-              
-        },
-              
-        green: {
-                
-        points: Math.floor(Math.random() * 12) + 1,
-                
-        imageUrl: "assets/images/green.png"
-         
+    green:
+    {
+      name: "Green",
+      value: 0
+    },
+    red:
+    {
+      name: "Red",
+      value: 0
+    },
+    yellow:
+    {
+      name: "Yellow",
+      value: 0
     }
-};
+  };
+  
